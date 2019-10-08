863fa6b2d1092468d1b242e2f2746e0cc9aeca70
Glitch (hello-webpage) <none>
2019-10-08 15:36:52 +0000

    ☃️💰 Checkpoint ./script.js:4364682/2217

--- script.js
+++ script.js
@@ -8,91 +8,11 @@ var tooltip = d3
   .append("div")
   .attr("class", "toolTip");
 
-/*
-var x = d3
-    .scaleBand()
-    .rangeRound([0, width])
-    .padding(0.1),
-  y = d3.scaleLinear().rangeRound([height, 0]);
-
-var colours = d3.scaleOrdinal().range(["#6F257F", "#CA0D59"]);
-
-var g = svg
-  .append("g")
-  .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-d3.json("data.json", function(error, data) {
-  if (error) throw error;
-
-  x.domain(
-    data.map(function(d) {
-      return d.txt;
-    })
-  );
-  y.domain([
-    0,
-    1,
-  ]);
-
-  g.append("g")
-    .attr("class", "axis axis--x")
-    .attr("transform", "translate(0," + height + ")")
-    .call(d3.axisBottom(x));
-
-  g.append("g")
-    .attr("class", "axis axis--y")
-    .call(
-      d3
-        .axisLeft(y)
-        .ticks(5)
-        .tickSizeInner([-width])
-    )
-    .append("text")
-    .attr("transform", "rotate(-90)")
-    .attr("y", 6)
-    .attr("dy", "0.71em")
-    .attr("text-anchor", "end")
-    .attr("fill", "#5D6971");
-
-  g.selectAll(".bar")
-    .data(data)
-    .enter()
-    .append("rect")
-    .attr("x", function(d) {
-      return x(d.txt);
-    })
-    .attr("y", function(d) {
-      return y(d.value);
-    })
-    .attr("width", x.bandwidth())
-    .attr("height", function(d) {
-      return height - y(d.value);
-    })
-    .attr("fill", function(d) {
-      return colours(d.txt);
-    })
-    .on("mouseenter", function(d) {
-    console.log("enter")
-      var str = "<img class=\"image\" src=\"" + d.img + "\"/>";
-      tooltip
-        .style("left", d3.event.pageX - 50 + "px")
-        .style("top", d3.event.pageY - 370 + "px")
-        .style("display", "inline-block")
-        .html(str)
-    })
-    .on("mouseleave", function(d) {
-      tooltip.style("display", "none");
-    });
-});
-*/
-
 var y = d3
     .scaleBand()
-    .rangeRound([0, width])
+    .rangeRound([0, height])
     .padding(0.1),
-  x = d3.scaleLinear().rangeRound([height, 0]);
-
-var colours = d3.scaleOrdinal().range(["#6F257F", "#CA0D59"]);
+  x = d3.scaleLinear().rangeRound([0, width]);
 
 var g = svg
   .append("g")
@@ -101,12 +21,12 @@ var g = svg
 d3.json("data.json", function(error, data) {
   if (error) throw error;
 
-  x.domain(
+  y.domain(
     data.map(function(d) {
       return d.txt;
     })
   );
-  y.domain([
+  x.domain([
     0,
     1,
   ]);
@@ -114,15 +34,13 @@ d3.json("data.json", function(error, data) {
   g.append("g")
     .attr("class", "axis axis--x")
     .attr("transform", "translate(0," + height + ")")
-    .call(d3.axisBottom(x));
+    .call(d3.axisBottom(x).ticks(5));
 
   g.append("g")
     .attr("class", "axis axis--y")
     .call(
       d3
         .axisLeft(y)
-        .ticks(5)
-        .tickSizeInner([-width])
     )
     .append("text")
     .attr("transform", "rotate(-90)")
@@ -135,18 +53,18 @@ d3.json("data.json", function(error, data) {
     .data(data)
     .enter()
     .append("rect")
-    .attr("x", function(d) {
-      return x(d.txt);
-    })
     .attr("y", function(d) {
-      return y(d.value);
+      return y(d.txt);
     })
-    .attr("width", x.bandwidth())
-    .attr("height", function(d) {
-      return height - y(d.value);
+    .attr("x2", function(d) {
+      return x(d.value);
+    })
+    .attr("height", y.bandwidth())
+    .attr("width", function(d) {
+      return width - x(d.value);
     })
     .attr("fill", function(d) {
-      return colours(d.txt);
+      return "lightblue"
     })
     .on("mouseenter", function(d) {
     console.log("enter")
