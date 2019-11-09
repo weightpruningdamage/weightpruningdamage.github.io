5abab009be1cb5fd19957fe97c706455bd742f7a
Glitch (hello-webpage) <none>
2019-11-09 00:10:24 +0000

    🌻🔨 Checkpoint ./script.js:8201077/3197 ./index.html:8201077/3143

--- script.js
+++ script.js
@@ -1,9 +1,3 @@
-var svg = d3.select("body").append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-  .append("g")
-    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
 var svg = d3.select("svg"),
   margin = { top: 20, right: 20, bottom: 30, left: 50 },
   width = +svg.attr("width") - margin.left - margin.right,
@@ -24,6 +18,16 @@ var g = svg
   .append("g")
   .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
+var xAxis = d3.svg.axis()
+    .scale(x)
+    .orient("bottom");
+
+var yAxis = d3.svg.axis()
+    .scale(y)
+    .orient("left")
+    .tickSize(0)
+    .tickPadding(6);
+
 d3.json("data.json", function(error, data) {
   if (error) throw error;
 
@@ -33,49 +37,44 @@ d3.json("data.json", function(error, data) {
     })
   );
   x.domain([
-    -10,
+    0,
     10,
   ]);
+
 
   g.append("g")
     .attr("class", "axis axis--x")
     .attr("transform", "translate(0," + height + ")")
-    .call(d3.axisBottom(x).ticks(5));
+    .call(xAxis);
 
   g.append("g")
     .attr("class", "axis axis--y")
-    .call(
-      d3
-        .axisLeft(y)
-    )
-
+    .attr("transform", "translate(" + x(0) + ",0)")
+      .call(yAxis);
+  
   g.selectAll(".bar")
-    .data(data)
-    .enter()
-    .append("rect")
-    .attr("y", function(d) {
-      return y(d.txt);
-    })
-    .attr("x2", function(d) {
-      return x(d.value);
-    })
-    .attr("height", y.bandwidth())
-    .attr("width", function(d) {
-      return width - x(d.value);
-    })
-    .attr("fill", function(d) {
-      return "lightblue"
-    })
-    .on("mouseenter", function(d) {
-      var str = "<img class=\"image\" src=\"" + d.img + "\"/>";
-      tooltip
-        .style("left", d3.event.pageX - 50 + "px")
-        .style("top", d3.event.pageY - 370 + "px")
-        .style("display", "inline-block")
-        .html(str)
-    })
-    .on("mouseleave", function(d) {
-      tooltip.style("display", "none");
-    });
+      .data(data)
+    .enter().append("rect")
+      .attr("class", function(d) { return "bar bar--" + (d.value < 0 ? "negative" : "positive"); })
+      .attr("x", function(d) { return x(Math.min(0, d.value)); })
+      .attr("y", function(d) { return y(d.name); })
+      .attr("width", function(d) { return Math.abs(x(d.value) - x(0)); })
+      .attr("height", y.rangeBand())
+      .on("mouseenter", function(d) {
+        var str = "<img class=\"image\" src=\"" + d.img + "\"/>";
+        tooltip
+          .style("left", d3.event.pageX - 50 + "px")
+          .style("top", d3.event.pageY - 370 + "px")
+          .style("display", "inline-block")
+          .html(str)
+      })
+      .on("mouseleave", function(d) {
+        tooltip.style("display", "none");
+      });
 });
+
+function type(d) {
+  d.value = +d.value;
+  return d;
+}
 
