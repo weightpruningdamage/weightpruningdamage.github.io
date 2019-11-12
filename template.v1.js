3a95c6396b5d23d2a712b044bb70f53c96e2eef4
Glitch (elderly-pine) <none>
2019-11-12 03:36:36 +0000

    🌓📹 Checkpoint ./index.html:8201077/1027 ./template.v1.js:8201077/243

--- template.v1.js
+++ template.v1.js
@@ -18242,8 +18242,9 @@ var btnContainer = document.getElementById("myBtnContainer");
 var btns = btnContainer.getElementsByClassName("btn");
 for (var i = 0; i < btns.length; i++) {
   btns[i].addEventListener("click", function(){
-    var current = document.getElementsByClassName("btn active");
-    current[0].className = current[0].className.replace("btn active", "btn");
+    var current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "btn");
     this.className += " active";
+    btns[i].style.color = "#666";
   });
 } 
