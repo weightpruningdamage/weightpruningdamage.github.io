af270b5c993a602b6d7a4cc37433f5215175f124
Glitch (elderly-pine) <none>
2019-11-14 21:59:45 +0000

    👵🚲 Checkpoint ./template.v1.js:8201077/41 ./script.js:8201077/3340 ./index.html:8201077/4344

--- template.v1.js
+++ template.v1.js
@@ -1,4 +1,5 @@
 // modified distill.pub template v1 for world models.
+
 (function (global, factory) {
 	typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) :
 	typeof define === 'function' && define.amd ? define(['exports'], factory) :
